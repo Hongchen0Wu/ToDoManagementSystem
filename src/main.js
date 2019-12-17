1ae42778cc5246d034110161c635cf31161dd0f8
Hongchen0Wu <1511690742@qq.com>
2019-12-17 22:01:01 +0800

    todoList by vue

---
+++ src/main.js
@@ -0,0 +1,22 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+import Element from 'element-ui';
+
+Vue.use(ElementUI);
+Vue.use(Element, { size: 'small', zIndex: 3000 });
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App),
+  components: { App },
+  template: '<App/>'
+})
